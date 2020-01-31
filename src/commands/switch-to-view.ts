cb35a3fd6149831e5b02722bdaf1ddc46dbf914f
Oleksandr Ostach <ukrorion@gmail.com>
2020-01-31 11:27:52 +0200

    Add option to define whether to open a view or show the list of all views

--- src/commands/switch-to-view.ts
+++ src/commands/switch-to-view.ts
@@ -1,13 +1,19 @@
 import * as path from 'path';
+import { CheckedSwitchFile } from '../types';
 import { getRailsContext } from '../rails-context';
 import { viewMaker } from '../makers';
 import { getSwitchesFromRule } from '../switches';
 import { openFile, showPicker } from './util';
 import { createView } from './create-view';
+import * as vscode from 'vscode';
 
 function isCurrentMethodView(methodName: string, filename: string): boolean {
   const viewName = path.basename(filename).split('.')[0];
   return viewName === methodName;
+}
+
+function filteredViewFiles(methodName: string, switchFiles: CheckedSwitchFile[]): CheckedSwitchFile[] {
+  return switchFiles.filter(file => isCurrentMethodView(methodName, file.filename));
 }
 
 export function switchToView(): Promise<void> {
@@ -17,6 +23,7 @@ export function switchToView(): Promise<void> {
     }
 
     const switchFiles = await getSwitchesFromRule(viewMaker, railsFile);
+    const openViewOnFirstMatch = vscode.workspace.getConfiguration('rails').get('openViewOnFirstMatch');
 
     if (switchFiles.length === 0) {
       return await createView();
@@ -27,8 +34,16 @@ export function switchToView(): Promise<void> {
       isCurrentMethodView(railsFile.methodName, switchFiles[0].filename)
     ) {
       return openFile(switchFiles[0].filename);
-    } else {
-      return showPicker(railsFile.railsRoot, switchFiles);
+    }
+
+    if ( switchFiles.length > 1 ) {
+      const viewFiles = filteredViewFiles(railsFile.methodName, switchFiles)
+
+      if (viewFiles.length == 1 || (openViewOnFirstMatch && viewFiles.length > 1) ) {
+        return openFile(viewFiles[0].filename);
+      } else {
+        return showPicker(railsFile.railsRoot, switchFiles);
+      }
     }
   });
 }
